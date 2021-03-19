437ee117c7a494217185fe7f3ebe731a0f5834c0
a150238 <eivind.askestad@nav.no>
2021-03-19 11:27:36 +0100

    MaskinportenEnvVariableConfigCreator using envkeys from  https://doc.nais.io/security/auth/maskinporten/

--- src/main/kotlin/no/nav/pensjon/opptjening/gcp/maskinporten/client/config/MaskinportenEnvVariableConfigCreator.kt
+++ src/main/kotlin/no/nav/pensjon/opptjening/gcp/maskinporten/client/config/MaskinportenEnvVariableConfigCreator.kt
@@ -3,33 +3,35 @@ package no.nav.pensjon.opptjening.gcp.maskinporten.client.config
 import com.nimbusds.jose.jwk.RSAKey
 import no.nav.pensjon.opptjening.gcp.maskinporten.client.exceptions.MaskinportenParseJwkException
 import no.nav.pensjon.opptjening.gcp.maskinporten.client.verifyEnvironmentVariables
+import java.net.URI
 
 
 class MaskinportenEnvVariableConfigCreator {
 
     companion object {
-        const val MASKINPORTEN_HOST_KEY = "MASKINPORTEN_HOST"
+        const val MASKINPORTEN_WELL_KNOWN_URL_KEY = "MASKINPORTEN_WELL_KNOWN_URL"
         const val MASKINPORTEN_CLIENT_ID_KEY = "MASKINPORTEN_CLIENT_ID"
-        const val MASKINPORTEN_JWK_PRIVATE_KEY = "MASKINPORTEN_JWK_PRIVATE_KEY"
-        const val MASKINPORTEN_SCOPE_KEY = "MASKINPORTEN_SCOPE"
+        const val MASKINPORTEN_CLIENT_JWK_KEY = "MASKINPORTEN_CLIENT_JWK"
+        const val MASKINPORTEN_SCOPES_KEY = "MASKINPORTEN_SCOPES"
         const val MASKINPORTEN_JWT_EXPIRATION_TIME_IN_SECONDS_KEY = "MASKINPORTEN_JWT_EXPIRATION_TIME_IN_SECONDS"
 
         private val requiredEnvKey = listOf(
-            MASKINPORTEN_HOST_KEY,
+            MASKINPORTEN_WELL_KNOWN_URL_KEY,
             MASKINPORTEN_CLIENT_ID_KEY,
-            MASKINPORTEN_JWK_PRIVATE_KEY,
-            MASKINPORTEN_SCOPE_KEY,
+            MASKINPORTEN_CLIENT_JWK_KEY,
+            MASKINPORTEN_SCOPES_KEY,
             MASKINPORTEN_JWT_EXPIRATION_TIME_IN_SECONDS_KEY
         )
 
         fun createMaskinportenConfig(env: Map<String, String>): MaskinportenConfig {
             env.verifyEnvironmentVariables(requiredEnvKey)
+            val wellKnownUrl = URI.create(env[MASKINPORTEN_WELL_KNOWN_URL_KEY]!!).toURL()
             return MaskinportenConfig(
-                baseUrl = env[MASKINPORTEN_HOST_KEY]!!,
+                baseUrl = "${wellKnownUrl.protocol}://${wellKnownUrl.host}",
                 clientId = env[MASKINPORTEN_CLIENT_ID_KEY]!!,
-                privateKey = parseJwk(env[MASKINPORTEN_JWK_PRIVATE_KEY]!!),
-                scope = env[MASKINPORTEN_SCOPE_KEY]!!,
-                validInSeconds = env[MASKINPORTEN_JWT_EXPIRATION_TIME_IN_SECONDS_KEY]!!.toInt()
+                privateKey = parseJwk(env[MASKINPORTEN_CLIENT_JWK_KEY]!!),
+                scope = env[MASKINPORTEN_SCOPES_KEY]!!,
+                validInSeconds = env.getOrDefault(MASKINPORTEN_JWT_EXPIRATION_TIME_IN_SECONDS_KEY, "120").toInt()
             )
         }
 
@@ -39,6 +41,4 @@ class MaskinportenEnvVariableConfigCreator {
             throw MaskinportenParseJwkException()
         }
     }
-
-
 }
--- src/test/kotlin/no/nav/pensjon/opptjening/gcp/maskinporten/client/config/MaskinportenEnvVariableConfigTest.kt
+++ src/test/kotlin/no/nav/pensjon/opptjening/gcp/maskinporten/client/config/MaskinportenEnvVariableConfigTest.kt
@@ -2,10 +2,10 @@ package no.nav.pensjon.opptjening.gcp.maskinporten.client.config
 
 import com.nimbusds.jose.jwk.gen.RSAKeyGenerator
 import no.nav.pensjon.opptjening.gcp.maskinporten.client.config.MaskinportenEnvVariableConfigCreator.Companion.MASKINPORTEN_CLIENT_ID_KEY
-import no.nav.pensjon.opptjening.gcp.maskinporten.client.config.MaskinportenEnvVariableConfigCreator.Companion.MASKINPORTEN_HOST_KEY
-import no.nav.pensjon.opptjening.gcp.maskinporten.client.config.MaskinportenEnvVariableConfigCreator.Companion.MASKINPORTEN_JWK_PRIVATE_KEY
+import no.nav.pensjon.opptjening.gcp.maskinporten.client.config.MaskinportenEnvVariableConfigCreator.Companion.MASKINPORTEN_WELL_KNOWN_URL_KEY
+import no.nav.pensjon.opptjening.gcp.maskinporten.client.config.MaskinportenEnvVariableConfigCreator.Companion.MASKINPORTEN_CLIENT_JWK_KEY
 import no.nav.pensjon.opptjening.gcp.maskinporten.client.config.MaskinportenEnvVariableConfigCreator.Companion.MASKINPORTEN_JWT_EXPIRATION_TIME_IN_SECONDS_KEY
-import no.nav.pensjon.opptjening.gcp.maskinporten.client.config.MaskinportenEnvVariableConfigCreator.Companion.MASKINPORTEN_SCOPE_KEY
+import no.nav.pensjon.opptjening.gcp.maskinporten.client.config.MaskinportenEnvVariableConfigCreator.Companion.MASKINPORTEN_SCOPES_KEY
 import no.nav.pensjon.opptjening.gcp.maskinporten.client.config.MaskinportenEnvVariableConfigCreator.Companion.createMaskinportenConfig
 import no.nav.pensjon.opptjening.gcp.maskinporten.client.exceptions.MaskinportenParseJwkException
 import no.nav.pensjon.opptjening.gcp.maskinporten.client.exceptions.MissingEnvironmentVariablesException
@@ -27,10 +27,10 @@ internal class MaskinportenEnvVariableConfigCreatorTest {
     fun `Throws MissingEnvironmentVariables when environment variables are missing`() {
         val exception = assertThrows<MissingEnvironmentVariablesException> { createMaskinportenConfig(emptyMap()) }
 
-        assertTrue(exception.message!! containWord MASKINPORTEN_HOST_KEY)
+        assertTrue(exception.message!! containWord MASKINPORTEN_WELL_KNOWN_URL_KEY)
         assertTrue(exception.message!! containWord MASKINPORTEN_CLIENT_ID_KEY)
-        assertTrue(exception.message!! containWord MASKINPORTEN_JWK_PRIVATE_KEY)
-        assertTrue(exception.message!! containWord MASKINPORTEN_SCOPE_KEY)
+        assertTrue(exception.message!! containWord MASKINPORTEN_CLIENT_JWK_KEY)
+        assertTrue(exception.message!! containWord MASKINPORTEN_SCOPES_KEY)
         assertTrue(exception.message!! containWord MASKINPORTEN_JWT_EXPIRATION_TIME_IN_SECONDS_KEY)
 
     }
@@ -40,22 +40,24 @@ internal class MaskinportenEnvVariableConfigCreatorTest {
         val envVariables = envVariables(createPrivateKey())
         val config: MaskinportenConfig = createMaskinportenConfig(envVariables)
 
-        assertEquals(envVariables[MASKINPORTEN_HOST_KEY],config.baseUrl)
+        assertEquals("https://dummyValue.com",config.baseUrl)
         assertEquals(envVariables[MASKINPORTEN_CLIENT_ID_KEY],config.clientId)
-        assertEquals(envVariables[MASKINPORTEN_JWK_PRIVATE_KEY],config.privateKey.toJSONString())
-        assertEquals(envVariables[MASKINPORTEN_SCOPE_KEY],config.scope)
+        assertEquals(envVariables[MASKINPORTEN_CLIENT_JWK_KEY],config.privateKey.toJSONString())
+        assertEquals(envVariables[MASKINPORTEN_SCOPES_KEY],config.scope)
         assertEquals(envVariables[MASKINPORTEN_JWT_EXPIRATION_TIME_IN_SECONDS_KEY]!!.toInt(),config.validInSeconds)
     }
 
     private fun envVariables(privateKey: String) = mapOf(
-        MASKINPORTEN_HOST_KEY to "https://dummyValue.com",
+        MASKINPORTEN_WELL_KNOWN_URL_KEY to "https://dummyValue.com/test",
         MASKINPORTEN_CLIENT_ID_KEY to "testClient",
-        MASKINPORTEN_JWK_PRIVATE_KEY to privateKey,
-        MASKINPORTEN_SCOPE_KEY to "testScope",
+        MASKINPORTEN_CLIENT_JWK_KEY to privateKey,
+        MASKINPORTEN_SCOPES_KEY to "testScope",
         MASKINPORTEN_JWT_EXPIRATION_TIME_IN_SECONDS_KEY to "120",
     )
 
 }
+// https://ver2.maskinporten.no/.well-known/oauth-authorization-server
+
 
 private infix fun String.containWord(word: String) = this.contains(word)
 
