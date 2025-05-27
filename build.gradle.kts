3ce8e7677edcd04e5b7a5f3c65a62fd3da8c5b07
Jan Kroken <jan.kroken@nav.no>
2025-05-27 13:36:30 +0200

    la på filtrering av prerelese-versjoner for dependencyUpdates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+import org.gradle.kotlin.dsl.withType
+
 plugins {
     kotlin("jvm") version libs.versions.kotlin.get()
     id("com.github.ben-manes.versions") version "0.52.0"
@@ -9,4 +12,14 @@ repositories {
 
 tasks.withType<Jar> {
     enabled = false //dont create jar for root
+}
+
+tasks.withType<DependencyUpdatesTask>().configureEach {
+    rejectVersionIf {
+        isNonStableVersion(candidate.version)
+    }
+}
+
+fun isNonStableVersion(version: String): Boolean {
+    return listOf("BETA","RC","-M",".CR").any { version.uppercase().contains(it) }
 }
